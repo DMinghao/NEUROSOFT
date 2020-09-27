16fa984dfecab68c58a2305e9327eb074f7b4987
SCP-D14134 <rentx@dukes.jmu.edu>
2020-09-26 23:34:57 -0400

    Update format and comment of middleware/auth.js

--- Main/backend/middleware/auth.js
+++ Main/backend/middleware/auth.js
@@ -2,22 +2,29 @@ const jwt = require("jsonwebtoken");
 
 const auth = (req, res, next) => {
   try {
+    // check if there is a token
     const token = req.header("x-auth-token");
-    if (!token)
+    if (!token) {
       return res
         .status(401)
-        .json({ msg: "No authentication token, authorization denied." });
+        .json({ msg: "No authentication token, authorization denied" });
+    }
 
+    // check if the token is verified
     const verified = jwt.verify(token, process.env.JWT_SECRET);
-    if (!verified)
+    if (!verified) {
       return res
         .status(401)
-        .json({ msg: "Token verification failed, authorization denied." });
+        .json({ msg: "Token authentication failed, authorization denied" });
+    }
 
     req.user = verified.id;
     next();
+
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    res
+      .status(500)
+      .json({ error: err.message });
   }
 };
 
