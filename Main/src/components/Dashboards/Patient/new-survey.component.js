38b0fec5dc79f4225ad55b2f9a3e55d66e717e57
DMinghao <minghaodu96@gmail.com>
2020-10-07 21:53:53 -0500

    Update new-survey.component.js

--- Main/src/components/Dashboards/Patient/new-survey.component.js
+++ Main/src/components/Dashboards/Patient/new-survey.component.js
@@ -9,43 +9,20 @@ import {template} from "./testSurveyTemp"
 export default function NewSurvey() {
   const history = useHistory();
   const { userData, setUserData } = useContext(UserContext);
-  // const [id, setId] = useState()
   if (!userData.user) history.push("/login");
 
-  // console.log(userData.token);
   Survey
     .StylesManager
     .applyTheme("default");
 
   const json = template
-
-  // setId(userData.user.id);
-  // console.log(userData.user); 
-
-  // const finish = (result) => {
-  //   const r = {
-  //     "user": userData.user.id,
-  //     "result": result.data
-  //   }
-  //   console.log(r);
-  //   // axios.post('http://localhost:5000/API/survey/add', r)
-  //   // axios.post('/API/survey/add', r, {headers : {"x-Auth-Token" : userData.token}})
-  //   axios.post('/API/survey/add', r)
-  //     .then(res => console.log(res.data))
-  //     .catch(error => console.log(error));
-
-  // }
-
   return (
-    // <Survey.Survey model={new Survey.Model(json)} onComplete={(result) => finish} />
     <Survey.Survey model={new Survey.Model(json)} onComplete={(result) => {
       const r = {
         "user": userData.user.id,
         "result": result.data
       }
       console.log(r);
-      // axios.post('http://localhost:5000/API/survey/add', r)
-      // axios.post('/API/survey/add', r, {headers : {"x-Auth-Token" : userData.token}})
       axios.post('/API/survey/add', r)
         .then(res => console.log(res.data))
         .catch(error => console.log(error));
