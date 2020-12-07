2cba4fd768d49b6ed38b5307eb9fd90119e05820
ReynaYC <reynayingc@gmail.com>
2020-12-07 10:47:25 -0600

    Survey_Distributio
    Survey Distribution changed

--- Main/backend/routes/API/distribution.js
+++ Main/backend/routes/API/distribution.js
@@ -2,12 +2,33 @@ const router = require('express').Router();
 const auth = require("../../middleware/auth");
 let Temp = require('../../models/survey/surveyTemp.model');
 let User = require('../../models/user.model');
+let surveyDis = require("../../models/survey/surveyDis.model")
 // let Page = require('../../models/survey/pageTemp.model');
 // let Question = require('../../models/survey/questionTemp.model');
 const mongoose = require('mongoose');
 
 
 //your code here
-
+router.post("/mydistribution", auth, async (req, res) => {
+    try {
+      const {docID} = req.body; 
+      var list = []
+      await Temp.find({'docID':docID}, (err, docs) =>{
+        list = docs
+      }).then(()=>{
+        list = list.map(x => {
+          const {_doc} = x
+          const {template, createdAt, updatedAt, __v, ...rest} = _doc
+          return rest
+        })
+        return res.status(200).json(list)
+      })
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: err.message });
+    }
+  
+  });
 
 module.exports = router;
