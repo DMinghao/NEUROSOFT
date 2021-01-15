30f19d1b0699a9dcf397fa5f9904785838f9c7ef
Zihan Wang <wswzh18@gmail.com>
2021-01-14 20:31:12 -0500

    Update distribution.js

--- Main/backend/routes/API/distribution.js
+++ Main/backend/routes/API/distribution.js
@@ -40,29 +40,57 @@ router.post("/add", auth, async (req, res) => {
 
 //your code here
 router.post("/mydistribution", auth, (req, res) => {
-    try {
-      const {docID} = req.body;
-      console.log(req.body)
-      var list = []
-      surveyDis.find({'docID':docID}, (err, docs) =>{
-        list = docs
-        console.log(docs)
-      }).then(()=>{
-        list = list.map(x => {
-          const {_doc} = x
-          const {template, createdAt, updatedAt, __v, ...rest} = _doc
-          return rest
-        })
-        return res.status(200).json(list)
+  try {
+    const { docID } = req.body;
+    console.log(req.body)
+    var list = []
+    surveyDis.find({ 'docID': docID }, (err, docs) => {
+      list = docs
+      console.log(docs)
+    }).then(() => {
+      list = list.map(x => {
+        const { _doc } = x
+        const { template, createdAt, updatedAt, __v, ...rest } = _doc
+        return rest
       })
-    } catch (error) {
-      console.log(error)
-      res
-        .status(500)
-        .json({ error: error.message });
+      return res.status(200).json(list)
+    })
+  } catch (error) {
+    console.log(error)
+    res
+      .status(500)
+      .json({ error: error.message });
+  }
+
+});
+    
+
+router.post("/pendingsurvey", auth, async (req, res) => {
+  try {
+    const { paID } = req.body;
+    const patient = await User.findById(paID);
+    var list = patient.relatedDist
+    var temlist = []
+    //console.log(list)
+    for (i = 0; i < list.length; i++){
+      var id = list[i]
+      const dist = await surveyDis.findById(id)
+      const palist = dist.patients
+      palist.forEach(pa => {
+        if (pa.paID == paID && pa.completed == false) {
+          var item = {distID: dist._id, tempID: dist.tempID}
+          temlist.push(item)
+          //console.log(dist.tempID)
+        }
+      })
     }
-  
-  });
-
-
+    res.status(200).json(temlist)
+  }
+  catch (error) {
+    console.log(error)
+    res
+      .status(500)
+      .json({ error: error.message });
+  }
+});
 module.exports = router;
