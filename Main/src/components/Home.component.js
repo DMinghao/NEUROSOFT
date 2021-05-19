1bc6cdc75bea74fefb8128352ceee274464a5e57
ReynaYC <reynayingc@gmail.com>
2021-05-18 21:42:27 -0500

    homepage updated

--- Main/src/components/Home.component.js
+++ Main/src/components/Home.component.js
@@ -26,23 +26,80 @@ return(
   </div>
   <div class="column1">
     <div class="intro">
-      <h4>Manage patient data through highly customizable surveys </h4>
+      <h3>Manage patient data through highly customizable surveys </h3>
       <br/>
       <p>With the help of AI and NLP technologies, surgeons can automatically generate medical reports with documented patient surveying.</p>
       <p>This application is built as a type of online responsive platform, where patients can fill out prepared surveys from doctors anywhere they want.</p>
+      <p>Doctors can then keep and extract patients’ records from the platform and spend less time reading and checking the paperwork during face-to-face meetings with patients. </p >
       {/* <a class="button button-ghost" href=""></a> */}
     </div>
   </div>
   
-</div>
-<div class="steps">
-    <img src={url2.default} alt="Doctors" width="20%" height="20%"/>
-    <img src={url3.default} alt="Patients" width="20%" height="20%"/>
-    <img src={url4.default} alt="Register" width="20%" height="20%"/>
-    <img src={url5.default} alt="Survey" width="20%" height="20%"/>
-    <img src={url6.default} alt="Send" width="20%" height="20%"/>
-    <img src={url7.default} alt="Report" width="20%" height="20%"/>
   </div>
+
+
+  <div className ="row">
+    <div className ="column2">
+      <img src={url2.default} alt="Patient" width="100%" height="100%" class="center"/>
+    </div>
+    <div className ="column2">
+      <br></br>
+      <br></br>
+      <br></br>
+      <img src={url5.default} alt="Patient" width="30%" height="30%" class="center"/>
+      <p>Create survey templates through feature-rich survey functions</p>
+    </div>
+    <div className ="column2">
+      <br></br>
+      <br></br>
+      <br></br>
+      <img src={url6.default} alt="Patient" width="30%" height="30%" class="center"/>
+      <p>Distribute surveys to patients</p>
+      <p>
+        Manage patient’s survey result
+      </p>
+    </div>
+    <div className ="column2">
+      <br></br>
+      <br></br>
+      <br></br>
+      <img src={url7.default} alt="Patient" width="30%" height="30%"class="center"/>
+      <p>Check Ai-powered auto-generated reports based on patient’s survey</p>
+    </div>
+  </div>
+  <div className ="row">
+    <div className ="column2">
+      <img src={url3.default} alt="Patient" width="100%" height="100%" class="center"/>
+    </div>
+    <div className ="column2">
+      <br></br>
+      <br></br>
+      <br></br>
+      <img src={url4.default} alt="Patient" width="30%" height="30%" class="center"/>
+      <p>Register/Log into the account</p>
+    </div>
+    <div className ="column2">
+      <br></br>
+      <br></br>
+      <br></br>
+      <img src={url5.default} alt="Patient" width="30%" height="30%" class="center"/>
+      <p>Fill out the survey you receive</p>
+      <p>
+        i. Complete the survey
+      </p>
+      <p>
+        ii. Save the survey, complete it later
+      </p>
+    </div>
+    <div className ="column2">
+      <br></br>
+      <br></br>
+      <br></br>
+      <img src={url7.default} alt="Patient" width="30%" height="30%"class="center"/>
+      <p>Keep the survey records in your account, then your doctor will know you better :)</p>
+    </div>
+  </div>
+
 </div>
 
 )
